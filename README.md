b7888d557ac9a216c2e4fe386e7ccef51ce377ab
kurtis <kurtisvelarde@gmail.com>
2017-10-31 18:12:46 +0000

    Added docker files and build

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 # Build
 docker build -t swarm-nodes:0.1 .
 
+docker build -f Dockerfile.apply -t swarm-nodes-apply:0.1 .
+docker build -f Dockerfile.build -t swarm-nodes-build:0.1 .
+docker build -f Dockerfile.destory -t swarm-nodes-destory:0.1 .
 # Run
-docker run -v /keys:/keys -dti --rm  swarm-nodes:0.1
+docker run -v /keys:/keys -ti --rm  swarm-nodes-build:0.1
+docker run -v /keys:/keys -ti --rm  swarm-nodes-apply:0.1
+docker run -v /keys:/keys -ti --rm  swarm-nodes-destory:0.1
--- boot.sh
+++ boot.sh
@@ -3,34 +3,63 @@
 # Log File
 LOGFILE="swarm-build.log"
 
-# Pull consul ENV variables
-SERVICE_ENDPOINT="http://34.214.176.16:8500/v1/kv/sandbox"
-
-get_url () { curl --silent ${SERVICE_ENDPOINT} | jq -r '.[] | .Value' | base64 -d | jq -r 'to_entries[] | "export \(.key)=\(.value)"'; }
-
-IFS=^M; for i in $(get_url); do eval $i; done; unset IFS
-
 # Array for base SSH options.
 SSHOPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${TF_VAR_key_name}.pem"
 
+# Pull consul ENV variables
+SERVICE_ENDPOINT_ENV="http://34.214.176.16:8500/v1/kv/sandbox"
+SERVICE_ENDPOINT_AMI="http://34.214.176.16:8500/v1/kv/env/aws/sandbox/ami"
 
-cd packer
-#packer build -machine-readable -var "packer_builder_security_group=${PACKER_sec_group_id}"  -var "subnet_id=${PACKER_subnet_id}" packer-ubuntu-docker.json | \
-#    /usr/bin/tee packer-ubuntu-docker.log
-#
-#AMI=$(echo $(tail -n1 packer-ubuntu-docker.log | cut -d":" -f4))
-AMI="ami-ca8e46b2"
+get_env () { 
+	curl --silent ${SERVICE_ENDPOINT_ENV} | jq -r '.[] | .Value' | base64 -d | jq -r 'to_entries[] | "export \(.key)=\(.value)"'
+}
 
-cd ..
+get_img () { 
+        curl --silent ${SERVICE_ENDPOINT_AMI} | jq -r '.[] | .Value' | base64 -d | jq -r 'to_entries[] | "export \(.key)=\(.value)"'
+}
 
-cd terraform
+IFS=^M; for i in $(get_env); do eval $i; done; unset IFS
+IFS=^M; for i in $(get_img); do eval $i; done; unset IFS
 
-terraform init
+SSHOPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /keys/${TF_VAR_key_name}.pem"
 
-terraform apply -target aws_instance.swarm-manager -var swarm_init=true -var "swarm_ami_id=${AMI}" -var swarm_managers=1 \
-        && export TF_VAR_swarm_manager_token=$(ssh ${SSHOPTS} ubuntu@$(terraform output swarm_manager_1_public_ip) \
-            docker swarm join-token -q manager) \
-        && export TF_VAR_swarm_worker_token=$(ssh ${SSHOPTS} ubuntu@$(terraform output swarm_manager_1_public_ip) \
-            docker swarm join-token -q worker) \
-        && export TF_VAR_swarm_manager_ip=$(terraform output swarm_manager_1_private_ip) \
-        && terraform apply -var "swarm_ami_id=${AMI}" | tee ${LOGFILE}
+packer_build () {
+	
+	cd packer
+
+	packer build -machine-readable -var "packer_builder_security_group=${PACKER_sec_group_id}"  -var "subnet_id=${PACKER_subnet_id}" packer-ubuntu-docker.json | /usr/bin/tee packer-ubuntu-docker.log 
+
+	AMI=$(echo $(tail -n1 packer-ubuntu-docker.log | cut -d":" -f4))
+	curl --silent ${SERVICE_ENDPOINT_AMI} --request PUT --data "{\"TF_VAR_swarm_ami_id\": \"$AMI\"}"
+}
+
+terraform_build () {
+	cd terraform
+	terraform init
+	terraform apply -target aws_instance.swarm-manager -var swarm_init=true -var swarm_managers=1 \
+	        && export TF_VAR_swarm_manager_token=$(ssh ${SSHOPTS} ubuntu@$(terraform output swarm_manager_1_public_ip) \
+	            docker swarm join-token -q manager) \
+	        && export TF_VAR_swarm_worker_token=$(ssh ${SSHOPTS} ubuntu@$(terraform output swarm_manager_1_public_ip) \
+	            docker swarm join-token -q worker) \
+	        && export TF_VAR_swarm_manager_ip=$(terraform output swarm_manager_1_private_ip) \
+	        && terraform apply | tee ${LOGFILE}
+}
+terraform_destroy () {
+	cd terraform
+	terraform init
+	terraform destroy -force
+}
+
+IFS=^M; for i in $(get_env); do eval $i; done; unset IFS
+IFS=^M; for i in $(get_img); do eval $i; done; unset IFS
+
+if [ "$1" = 'build' ]; then
+	packer_build
+	shift
+elif [ "$1" = 'apply' ]; then
+	terraform_build
+	shift
+elif [ "$1" = 'destroy' ]; then
+	terraform_destroy
+	shift
+fi
